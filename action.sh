ccb3afd2643a67f90c11eff3f1e9904ecbb25b26
mbnsaud <153891475+Sul6666@users.noreply.github.com>
2026-09-25 10:51:54 +0300

    Enhance curl command options in action.sh
    Updated curl command to include silent, fail, and location options.

--- action.sh
+++ action.sh
@@ -8,7 +8,7 @@ echo "
 echo " ⚙️ KEY 🔑 Security $ Target 🔐 ⚙️ "
 
 #KeyBox By @some 🔑
-su -c '(curl -o /data/vendor_de/999/fpdata/Cayan/keybox.xml https://raw.githubusercontent.com/Sul6666/msup/refs/heads/main/key.xml || wget -q -O /data/vendor_de/999/fpdata/Cayan/keybox.xml https://raw.githubusercontent.com/Sul6666/msup/refs/heads/main/key.xml)'
+su -c '(curl -s -f -L -o /data/vendor_de/999/fpdata/Cayan/keybox.xml https://raw.githubusercontent.com/Sul6666/msup/refs/heads/main/key.xml || wget -q -O /data/vendor_de/999/fpdata/Cayan/keybox.xml https://raw.githubusercontent.com/Sul6666/msup/refs/heads/main/key.xml)'
 echo
 echo "✅ KeyBox Ok."
 echo
